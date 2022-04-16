08f176704c6e8edcfc585e82befeb6466f6c54b0
qiyii <66106591+dao0@users.noreply.github.com>
2022-04-16 01:47:02 -0600

    Update Contact.js

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,10 +1,44 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+
+  padding: theme.spacing(15),
+  textAlign: 'center',
+
+}));
+
 const Contact = () => {
-    return (
-      <div>
-        Contact Page
-      </div>
-    );
-  };
-  
-  export default Contact;
-  
+  return (
+    <div>
+      <Box sx={{
+        mt: 2,
+        mx: 2,
+        width: 1,
+        border: 0,
+        boxShadow: 0 
+      }}>
+        <Grid container rowSpacing={8} columnSpacing={7}>
+          <Grid item xs={12} sm={4.5}>
+            <Item>
+              info
+            </Item>
+          </Grid>
+          <Grid item xs={12} sm={7.5}>
+            <Item>
+              map
+            </Item>
+          </Grid>
+          <Grid item xs={12}>
+            <Item>
+              contact form
+            </Item>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
+}
