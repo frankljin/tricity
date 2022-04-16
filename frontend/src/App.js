f38c6a3704aa20d44b6d10a7e118e08c668df66e
frankljin <52306663+frankljin@users.noreply.github.com>
2022-04-15 22:47:19 -0400

    item content type + fetch from frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:1337/api/items?populate=*").then((res) => {
+      setItems(res?.data?.data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {items.map((item) => {
+        const itemAttributes = item.attributes;
+        return (
+          <div>
+            <p>{JSON.stringify(itemAttributes)}</p>
+      
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
