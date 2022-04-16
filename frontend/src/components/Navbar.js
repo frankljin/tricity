655673724694ae9dc521cb3d955799f15178b058
frankljin <52306663+frankljin@users.noreply.github.com>
2022-04-16 13:33:13 -0400

    basic nav

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,14 +1,33 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 
 import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
   return (
     <div className={styles.navBackground}>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/menu">Menu</Link>
-      <Link to="/contact">Contact</Link>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            sx={{ flexGrow: 1 }}
+            component={NavLink}
+            to={"/"}
+            style={{ textDecoration: "none", color: "unset" }}
+          >
+            Tricity
+          </Typography>
+          <Button color="inherit" component={NavLink} to={"/about"}>
+            About
+          </Button>
+          <Button color="inherit" component={NavLink} to={"/menu"}>
+            Menu
+          </Button>
+          <Button color="inherit" component={NavLink} to={"/contact"}>
+            Contact
+          </Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
