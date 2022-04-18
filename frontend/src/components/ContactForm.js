a8e946dc90a538b432a92ddc26b6ec38f4fb4ca6
Jason <jason1382311@gmail.com>
2022-04-17 22:00:44 -0700

    emailjs works and error message appear when input is not corect

--- frontend/src/components/ContactForm.js
+++ frontend/src/components/ContactForm.js
@@ -1,72 +1,75 @@
 import { makeStyles } from "@material-ui/styles"
-// import { useState } from 'react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-// import { ToastContainer, toast } from 'react-toastify';
-//
-// import emailjs from 'emailjs-com';
+import { ToastContainer, toast } from 'react-toastify';
+
+import emailjs from 'emailjs-com';
 import 'react-toastify/dist/ReactToastify.min.css';
 
-import { Grid, TextField, Button, CardContent, Typography } from '@material-ui/core';
+import { Grid, TextField, Button, CardContent} from '@material-ui/core';
 function ContactForm() {
     const {
-    // register,
-    // handleSubmit,
-    //reset,
-    // formState: { errors }
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors }
     } = useForm();
-    // const [disabled, setDisabled] = useState(false);
+    const [disabled, setDisabled] = useState(false);
 
     // Function that displays a success toast on bottom right of the page when form submission is successful
-    // const toastifySuccess = () => {
-    //     toast('Form sent!', {
-    //     position: 'bottom-right',
-    //     autoClose: 5000,
-    //     hideProgressBar: true,
-    //     closeOnClick: true,
-    //     pauseOnHover: true,
-    //     draggable: false,
-    //     className: 'submit-feedback success',
-    //     toastId: 'notifyToast'
-    //     });
-    // };
-    // const onSubmit = async (data) => {
+    const toastifySuccess = () => {
+        toast('Form sent!', {
+        position: 'bottom-right',
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: false,
+        className: 'submit-feedback success',
+        toastId: 'notifyToast'
+        });
+    };
+    const onSubmit = async (data) => {
 
-    // // Destrcture data object
-    // const { name, email, subject, message } = data;
-    // try {
-    //   // Disable form while processing submission
-    //   setDisabled(true);
+    // Destrcture data object
+    const { name, email, subject, message } = data;
+    try {
+      // Disable form while processing submission
+      setDisabled(true);
 
-    //   // Define template params
-    //   const templateParams = {
-    //     name,
-    //     email,
-    //     subject,
-    //     message
-    //   };
+      // Define template params
+      const templateParams = {
+        name,
+        email,
+        subject,
+        message
+      };
 
-    //   // Use emailjs to email contact form data
-    //   await emailjs.send(
-    //     process.env.REACT_APP_SERVICE_ID,
-    //     process.env.REACT_APP_TEMPLATE_ID,
-    //     templateParams,
-    //     process.env.REACT_APP_USER_ID
-    //   );
+      // Use emailjs to email contact form data
+      await emailjs.send(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        templateParams,
+        process.env.REACT_APP_USER_ID
+      );
 
-    //   // Reset contact form fields after submission
-    //   reset();
-    //   // Display success toast
-    //   toastifySuccess();
-    //   // Re-enable form submission
-    //   setDisabled(false);
-    // } catch (e) {
-    //   console.log(e);
-    // }
-  //};
+      // Reset contact form fields after submission
+      reset();
+      // Display success toast
+      toastifySuccess();
+      // Re-enable form submission
+      setDisabled(false);
+    } catch (e) {
+      console.log(e);
+    }
+  };
     const useStyles = makeStyles(() => ({
         root: {
             // height: '100vh',
             // width: "60vw"
+        },
+        error: {
+          color:"red"
         }
     }))
     const classes = useStyles();
@@ -74,25 +77,80 @@ function ContactForm() {
         <div className = {classes.root}>     
           <CardContent>
             <h1>Contact Us</h1>
-            <form>
+            <form id='contact-form' onSubmit={handleSubmit(onSubmit)} noValidate>
               <Grid container spacing={1}>
                 <Grid xs={12} sm={6} item>
-                  <TextField required name = "name" placeholder="First name" label="First Name" variant="outlined" fullWidth  />
+                  <TextField required name = "name" placeholder="First name" label="First Name" variant="outlined" fullWidth
+                  {...register('name', {
+                        required: {
+                          value: true,
+                          message: 'Please enter your name'
+                        },
+                        maxLength: {
+                          value: 30,
+                          message: 'Please use 30 characters or less'
+                        }
+                      })}  />
+                      {errors.name && (
+                      <span className={classes.error}>Please enter a valid name</span>
+                    )}
                 </Grid>
                 <Grid xs={12} sm={6} item>
-                  <TextField required name = "name" placeholder="Last name" label="Last Name" variant="outlined" fullWidth />
+                  <TextField required name = "name" placeholder="Last name" label="Last Name" variant="outlined" fullWidth 
+                  {...register('name', {
+                        required: {
+                          value: true,
+                          message: 'Please enter your name'
+                        },
+                        maxLength: {
+                          value: 30,
+                          message: 'Please use 30 characters or less'
+                        }
+                      })}/>
+                      {errors.name && (
+                      <span className={classes.error}>Please enter a valid name</span>
+                    )}
+                      
                 </Grid>
                 <Grid item xs={12}>
-                  <TextField required  name = "email" type="email" placeholder="Email" label="Email" variant="outlined" fullWidth/>
+                  <TextField required  name = "email" type="email" placeholder="Email" label="Email" variant="outlined" fullWidth
+                  {...register('email', {
+                        required: true,
+                        pattern:
+                          /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                      })}/>
+                      {errors.email && (
+                      <span className={classes.error}>Please enter a valid email address</span>
+                    )}
                 </Grid>
                 <Grid item xs={12}>
-                  <TextField required  name = "subject" placeholder="Subject" label="Subject" variant="outlined" fullWidth />
+                  <TextField required  name = "subject" placeholder="Subject" label="Subject" variant="outlined" fullWidth 
+                  {...register('subject', {
+                        required: {
+                          value: true,
+                          message: 'Please enter a subject'
+                        },
+                        maxLength: {
+                          value: 75,
+                          message: 'Subject cannot exceed 75 characters'
+                        }
+                      })}/>
+                      {errors.subject && (
+                      <span className={classes.error}>{errors.subject.message}</span>
+                    )}
+                      
                 </Grid>
                 <Grid item xs={12}>
-                  <TextField required name = "message" label="Message" multiline rows={3} placeholder="Message" variant="outlined" fullWidth  />
+                  <TextField required name = "message" label="Message" multiline rows={3} placeholder="Message" variant="outlined" fullWidth 
+                   {...register('message', {
+                        required: true
+                      })}/>{errors.subject && (
+                      <span className={classes.error}>{errors.subject.message}</span>
+                    )}
+                      
                 </Grid>
                 <Grid item xs={12}>
-                  <Button type="submit" variant="contained"  fullWidth>Submit</Button>
+                  <Button disabled = {disabled} type="submit" variant="contained"  fullWidth>Submit</Button>
                 </Grid>
 
               </Grid>
