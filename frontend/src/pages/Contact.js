d139a449282612cb5e4e463f80c7e54a7e9f4cf5
qiyii <66106591+dao0@users.noreply.github.com>
2022-04-18 00:39:14 -0600

    Update Contact.js

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -22,7 +22,7 @@ const Contact = () => {
       }}>
         <Grid container rowSpacing={1} columnSpacing={1} alignItems="center">
           <Grid item xs={12} sm={5} style={{height: "50vh"}}>
-            <Typography variant="h3" mt={0}>
+            <Typography variant="h3" component="h3" mt={0}>
               Location
             </Typography>
             <p>Tricity Japanese Salmon Bowl offers an authentic taste of Japan, serving an assortment of beautifully balanced donburi and other classics of the cuisine. Located just off the bustling Highway 24, this hidden gem of Cambridge boasts freshly sourced ingredients, served in a familiar setting. Situated in a prime central location with seamless transit access, you won’t want to miss the taste of Japan here!</p>
